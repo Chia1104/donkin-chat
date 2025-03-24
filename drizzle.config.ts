2bf3151055d2ca4dc9a7d44ebbe332f1d2dc76f9
Chia1104 <yuyuchia7423@gmail.com>
2025-03-24 10:59:21 +0800

    fix: update dependencies

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-	dialect: 'postgresql',
-	schema: './src/database/server/schema',
-	dbCredentials: {
-		url: process.env.DATABASE_URL!,
-	},
-});
