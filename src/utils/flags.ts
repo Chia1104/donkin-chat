1263ab2895e079354b12e1c255b8ca6207bebcfb
Chia1104 <yuyuchia7423@gmail.com>
2025-03-26 22:22:49 +0800

    fix: update flags key

--- src/utils/flags.ts
+++ src/utils/flags.ts
@@ -7,10 +7,14 @@ import { tryCatch } from './try-catch';
 
 const getConfig = async (id: string) => get(id);
 
+export const FlagID = {
+	AI_CHAT: 'flags-ai-chat',
+} as const;
+
 export const aiChatFlag = flag<boolean>({
-	key: 'flags.ai-chat',
+	key: FlagID.AI_CHAT,
 	async decide() {
-		const { data: config, error } = await tryCatch(getConfig('flags.ai-chat'));
+		const { data: config, error } = await tryCatch(getConfig(FlagID.AI_CHAT));
 
 		if (!config || error) {
 			return env.FLAGS_AI_CHAT === 'true' && !!env.OPENAI_API_KEY;
