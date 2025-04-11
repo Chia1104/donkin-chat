ffcdb4f5c18c4b70c84bfa3da31ae17e5638d64c
Chia1104 <yuyuchia7423@gmail.com>
2025-04-11 19:50:09 +0800

    feat: kol resources

--- src/libs/kol/resources/kol.resource.ts
+++ src/libs/kol/resources/kol.resource.ts
@@ -1,6 +1,18 @@
+import { z } from 'zod';
+
+import { kolAlertPipe } from '@/libs/kol/pipes/kol.pipe';
+import type { KolAlert } from '@/libs/kol/pipes/kol.pipe';
+import type { BaseRequestOptions, ResponseData } from '@/types/request';
 import { request } from '@/utils/request';
 
-export const getKol = async () => {
-	const response = await request().get('/kol');
-	return response.json();
+export const getKolAlerts = async (options?: BaseRequestOptions<{ token_address: string }>) => {
+	const response = await request({ requestMode: 'node-endpoint' })
+		.get('api/v1/kol_alerts_by_token', {
+			searchParams: {
+				token_address: options?.data?.token_address ?? '',
+			},
+		})
+		.json<ResponseData<KolAlert[]>>();
+
+	return z.array(kolAlertPipe).parse(response.data);
 };
