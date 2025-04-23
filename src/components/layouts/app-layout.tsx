09760fa4c06db6485e23bcb9f80cd6dea852945c
Chia1104 <yuyuchia7423@gmail.com>
2025-04-23 16:43:36 +0800

    fix: update fonts

--- src/components/layouts/app-layout.tsx
+++ src/components/layouts/app-layout.tsx
@@ -2,8 +2,9 @@ import type { ReactNode, FC, ComponentPropsWithoutRef } from 'react';
 
 import { ViewTransitions } from 'next-view-transitions';
 
+import { Locale } from '@/enums/locale.enum';
 import { Theme } from '@/enums/theme.enum';
-import { noto_sans_tc, noto_sans_sc } from '@/themes/fonts';
+import { noto_sans_tc, noto_sans_sc, noto_sans } from '@/themes/fonts';
 import { cn } from '@/utils/cn';
 
 interface Props {
@@ -16,13 +17,26 @@ interface Props {
 
 const AppLayout: FC<Props> = ({ children, locale, htmlProps, bodyProps, theme }) => {
 	const themeSchema = theme ? (theme === Theme.Dark ? Theme.Dark : Theme.Light) : undefined;
+	const fontFamily =
+		locale === Locale.ZH_TW
+			? noto_sans_tc.className
+			: locale === Locale.ZH_CN
+				? noto_sans_sc.className
+				: noto_sans.className;
 	return (
 		<ViewTransitions>
 			<html
 				lang={locale}
 				suppressHydrationWarning
 				{...htmlProps}
-				className={cn(htmlProps?.className, themeSchema, noto_sans_tc.variable, noto_sans_sc.variable)}
+				className={cn(
+					htmlProps?.className,
+					themeSchema,
+					fontFamily,
+					noto_sans_tc.variable,
+					noto_sans_sc.variable,
+					noto_sans.variable,
+				)}
 				style={{
 					colorScheme: themeSchema,
 					...htmlProps?.style,
--- src/themes/fonts.ts
+++ src/themes/fonts.ts
@@ -1,5 +1,5 @@
 import { Noto_Sans_TC, Noto_Sans_SC, Noto_Sans } from 'next/font/google';
 
-export const noto_sans_tc = Noto_Sans_TC({ weight: '300', subsets: ['latin'], variable: '--font-noto-sans-tc' });
-export const noto_sans_sc = Noto_Sans_SC({ weight: '300', subsets: ['latin'], variable: '--font-noto-sans-sc' });
-export const noto_sans = Noto_Sans({ weight: '300', subsets: ['latin'], variable: '--font-noto-sans' });
+export const noto_sans_tc = Noto_Sans_TC({ weight: '300', variable: '--font-noto-sans-tc' });
+export const noto_sans_sc = Noto_Sans_SC({ weight: '300', variable: '--font-noto-sans-sc' });
+export const noto_sans = Noto_Sans({ weight: '300', variable: '--font-noto-sans' });
