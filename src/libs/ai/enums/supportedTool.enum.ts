a97f71e9fa2c4b3b71c218132cd9de404f4f307c
Chia1104 <yuyuchia7423@gmail.com>
2025-04-26 22:55:27 +0800

    feat: ask ai token trade info

--- src/libs/ai/enums/supportedTool.enum.ts
+++ src/libs/ai/enums/supportedTool.enum.ts
@@ -1,6 +1,8 @@
 export const SupportedTool = {
 	GetTokenInfo: 'get_token_info',
 	GetTokenTrend: 'get_token_trend',
+	Get7DKOLAlerts: 'get_7d_kol_alerts',
+	Get7DSmartMoneyTrades: 'get_7d_smart_money_trades',
 } as const;
 
 export type SupportedTool = (typeof SupportedTool)[keyof typeof SupportedTool];
